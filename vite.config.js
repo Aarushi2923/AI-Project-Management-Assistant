4a6f69e532b50d4b5bafd04de71b1b96f02bd337
Aarushi <aarushi.011004@gmail.com>
2025-08-28 15:41:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Edit `base` before deploying to GitHub Pages if needed
 export default defineConfig({
   plugins: [react()],
-  base: ''
+  base: '/AI-PM-Assistant-React/'
 })
